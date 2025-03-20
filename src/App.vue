35e91a5e481f56c6c0fe3f0d5cf769bc72eac56e
Pedro Fonseca <pedrofonseca447@gmail.com>
2025-03-20 19:15:29 -0300

    chamada de api usando axios configurada

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+<div>
+  <h1>OLAAAAAAA</h1>
+
+
+  <input type="radio" name="options" value="True">
+  <label>True</label><br>
+
+  <input type="radio" name="options" value="False">
+  <label>False</label><br>
+
+  <button class="send" type="button">Send</button>
+
+</div> 
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  
+ created(){
+
+
+      this.axios
+      .get('https://opentdb.com/api.php?amount=1')
+      .then((response) => {
+        console.log(response.data.results[0])
+      })
+
+ }
+ 
+}//na template fica tudo que é visivel na pagina
+
+//toda a logica desse componmente
+
+
+//https://opentdb.com/api.php?amount=1 api de perguntas
 </script>
 
-<style>
+<style lang="scss">
 #app {
+  
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type=radio]{
+    margin: 12px 4px;
+  }
+
+  button.send{
+        color: white;
+        background-color:#1867c0;
+        border: 1px solid #1867c0;
+        cursor: pointer;
+        min-width: 120px;
+        height: 40px;
+        margin-top: 12px;
+        padding: 0;
+  }
 }
 </style>
